bf955070c84e330ad124cd7dd3bc32ab53c40e1e
vonas <24623262+vonas@users.noreply.github.com>
2020-08-18 21:07:08 +0200

    Change __title__ to simon

--- simon/__init__.py
+++ simon/__init__.py
@@ -8,7 +8,7 @@ A bot for searching Yu-Gi-Oh! cards.
 
 """
 
-__title__ = 'simonbot'
+__title__ = 'simon'
 __author__ = 'Jonas van den Berg'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020 Jonas van den Berg'
